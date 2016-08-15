e8e3312012371a2f14aee5fd5d74b85181888420
Karan <karan.s1992@outlook.com>
2016-08-15 12:39:17 -0700

    Update BTMirrorTree.java

--- src/karan/tree/BTMirrorTree.java
+++ src/karan/tree/BTMirrorTree.java
@@ -5,20 +5,31 @@ import karan.Node.BTNode;
 
 public class BTMirrorTree {
 
-	public static void  doMirror(BTNode root){
-		
-	}
-	public static void main(String[] args) {
-		BinarySearchTree bst = new BinarySearchTree();
-		bst.insert(5);
-		bst.insert(3);
-		bst.insert(4);
-		bst.insert(2);
-		bst.insert(7);
-		bst.insert(8);
-		bst.insert(6);
-		bst.displayInorder(bst.getRoot());
-		System.out.println("****");
-		bst.displayInorder(bst.getRoot());
-	}
+	public static BTNode doMirror(BTNode root){
+        if(root == null){
+            return root;
+        }else{
+            BTNode naya = new BTNode();
+            naya.setValue(root.getValue());
+            naya.setRight(doMirror(root.getLeft()));
+            naya.setLeft(doMirror(root.getRight()));
+            return  naya;
+        }
+    }
+
+    public static void main(String[] args) {
+        BST bst = new BST();
+        bst.insert(5);
+        bst.insert(3);
+        bst.insert(4);
+        bst.insert(2);
+        bst.insert(7);
+        bst.insert(8);
+        bst.insert(6);
+        bst.displayInorder(bst.getRoot());
+        System.out.println("***-*");
+        BTNode temp  = doMirror(bst.getRoot());
+        temp.printInorder(temp);
+
+    }
 }
